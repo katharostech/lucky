22f1162e2bafd2bb0a6e4f47086b43f2eccc6f34
zicklag <zicklag@katharostech.com>
2019-11-02 20:23:07 +0000

    Bundle Charm Template Into the Binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::io::stdout().write_all(include_bytes!(concat!(
+        env!("OUT_DIR"),
+        "/charm_template.zip"
+    )))?;
+
+    Ok(())
 }
