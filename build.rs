21c678137e9782b6a17c37353c2f3f63eac1acf6
zicklag <zicklag@katharostech.com>
2019-11-02 22:18:29 +0000

    Get Charm Template to Extract From Binary

--- build.rs
+++ build.rs
@@ -1,23 +1,19 @@
 use std::env;
-use std::io::prelude::*;
-use std::io::Write;
-use zip::write::FileOptions;
-
 use std::fs::File;
+use std::io::{prelude::*, Write};
 use std::path::Path;
+
 use walkdir::WalkDir;
 
 fn main() {
-    // For now tar is required on the system compiling the project
     let manifest_dir_path = format!("{}/charm_template", env::var("CARGO_MANIFEST_DIR").unwrap());
     let charm_template_zip_path = format!("{}/charm_template.zip", env::var("OUT_DIR").unwrap());
     let prefix = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     let file_writer = File::create(&charm_template_zip_path).unwrap();
     let mut zip = zip::ZipWriter::new(file_writer);
-    let options = FileOptions::default()
-        .compression_method(zip::CompressionMethod::Bzip2)
-        .unix_permissions(0o755);
+    let options =
+        zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Bzip2);
 
     let dir_iter = WalkDir::new(&manifest_dir_path).into_iter();
 
@@ -25,14 +21,9 @@ fn main() {
     for entry in dir_iter {
         let entry = entry.unwrap();
         let path = entry.path();
-        eprintln!("{:?}", path);
-        eprintln!("{:?}", prefix);
         let name = path.strip_prefix(Path::new(&prefix)).unwrap();
 
-        // Write file or directory explicitly
-        // Some unzip tools unzip files with directory paths correctly, some do not!
         if path.is_file() {
-            println!("adding file {:?} as {:?} ...", path, name);
             zip.start_file_from_path(name, options).unwrap();
             let mut f = File::open(path).unwrap();
 
@@ -40,9 +31,6 @@ fn main() {
             zip.write_all(&*buffer).unwrap();
             buffer.clear();
         } else if name.as_os_str().len() != 0 {
-            // Only if not root! Avoids path spec / warning
-            // and mapname conversion failed error on unzip
-            println!("adding dir {:?} as {:?} ...", path, name);
             zip.add_directory_from_path(name, options).unwrap();
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
-use std::io::Write;
+use std::fs;
+use std::io;
+
+const CHARM_TEMPLATE_ARCHIVE: &[u8] =
+    include_bytes!(concat!(env!("OUT_DIR"), "/charm_template.zip"));
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    std::io::stdout().write_all(include_bytes!(concat!(
-        env!("OUT_DIR"),
-        "/charm_template.zip"
-    )))?;
+    let zip_reader = std::io::Cursor::new(CHARM_TEMPLATE_ARCHIVE);
+    let mut zip = zip::ZipArchive::new(zip_reader)?;
+
+    for i in 0..zip.len() {
+        let mut file = zip.by_index(i).unwrap();
+        let outpath = file.sanitized_name();
+
+        if (&*file.name()).ends_with('/') {
+            fs::create_dir_all(&outpath).unwrap();
+        } else {
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(&p).unwrap();
+                }
+            }
+            let mut outfile = fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+
+        // Get and Set permissions
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+
+            if (&*file.name()).contains("hooks") {
+                // Make hooks executable
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(0o744))?;
+            } else if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode))?;
+            }
+        }
+    }
 
     Ok(())
 }
