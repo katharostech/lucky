6912bf8357530e842393dd59e4ff2af703c1471f
zicklag <zicklag@katharostech.com>
2019-11-14 02:07:09 +0000

    Add Client Connection Args

--- src/cli/client.rs
+++ src/cli/client.rs
@@ -1,11 +1,12 @@
 // use anyhow::Context;
-use clap::{App, ArgMatches};
+use clap::{App, Arg, ArgMatches};
 
 // mod start;
 // mod run_hook;
 
 use crate::cli::doc;
 
+#[rustfmt::skip]
 /// Return the `client` subcommand
 pub(crate) fn get_subcommand<'a>() -> App<'a> {
     crate::cli::new_app("client")
@@ -13,6 +14,31 @@ pub(crate) fn get_subcommand<'a>() -> App<'a> {
         // .subcommand(start::get_subcommand())
         // .subcommand(run_hook::get_subcommand())
         .arg(doc::get_arg())
+        // TODO: These are the exact same arguments as for the `daemon` subcommand:
+        // we should probably remove the code duplication.
+        .arg(Arg::with_name("unit_name")
+            .long("unit-name")
+            .short('u')
+            .help("The name of the Juju unit that this daemon is running for")
+            .long_help(concat!(
+                "The name of the Juju unit that this daemon is running for. This will be used to ",
+                "determine path to the socket to listen on. For example a unit name of ",
+                r#""mysql/2" would listen on the socket "/run/lucky_mysql_2.sock"."#
+            ))
+            .takes_value(true)
+            .env("JUJU_UNIT_NAME")
+            .required_unless("socket_path"))
+        .arg(Arg::with_name("socket_path")
+            .long("socket-path")
+            .short('s')
+            .help("The path to the socket to listen on")
+            .long_help(concat!(
+                "The path to the socket to listen on. This will override the path determined by ",
+                "the unit-name argument."
+            ))
+            .takes_value(true)
+            .required_unless("unit_name")
+            .env("LUCKY_DAEMON_SOCKET"))
 }
 
 /// Run the `client` subcommand
