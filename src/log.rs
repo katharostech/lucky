d2e1b1e76267add636ce4d46582cedad14b14cfb
Zicklag <zicklag@katharostech.com>
2019-11-16 21:49:46 +0000

    Send Daemon Logs to juju-log When Available

--- src/log.rs
+++ src/log.rs
@@ -1,4 +1,5 @@
 use log::{Level, Metadata, Record};
+use std::process::Command;
 
 /// Logger used for the Lucky daemon
 pub(crate) struct DaemonLogger;
@@ -10,6 +11,21 @@ impl log::Log for DaemonLogger {
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
+            // Log to Juju
+            let mut cmd = Command::new("juju-log");
+            if record.level() == Level::Debug {
+                cmd.arg("--debug");
+            }
+            cmd.arg(format!("{}", record.args()));
+
+            cmd.spawn().map_err(|e| {
+                match e.kind() {
+                    std::io::ErrorKind::NotFound => (), // Ignore it if juju-log isn't in the path
+                    _ => eprintln!("[lucky::log][WARN]: Could not log to juju-log: {}", e)
+                }
+            }).ok();
+
+            // Log to standard out
             eprintln!(
                 "[{}][{}]: {}",
                 record.target(),
