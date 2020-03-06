ca23624f0f6469f7f0dad8c61f306e45248da65e
Zicklag <zicklag@katharostech.com>
2020-03-06 01:58:09 +0000

    commit c724dcf2b2dbd81a14cf5b417c0e577f0bd1dc67 Author: Zicklag <zicklag@katharostech.com> Date:   Fri Mar 6 01:46:06 2020 +0000
        Fix Bug With CodiMD Example Charm
    
        There is some goofy stuff happening with the port and domain of the
        example charm. I added a note about it in the README and changed some
        config to help address it.

---
+++ codimd-example-charm/host_scripts/handle-website-relation.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+# Here we match on the first argument ( $1 ) passed in from the lucky.yaml
+
+# If we are joining a new relation
+if [ "$1" = "join" ]; then
+    lucky set-status maintenance "Joining HTTP relation"
+
+    # Set hostname and port values for the joined relation
+    lucky relation set \
+        "hostname=$(lucky private-address)" \
+        "port=$(lucky get-config port)"
+
+# If we are supposed to update our existing relations
+elif [ "$1" = "update" ]; then
+    lucky set-status maintenance "Updating HTTP relations"
+
+    # For every appliacation connected to or "website" relation
+    for relation_id in $(lucky relation list-ids website); do
+        # Set the hostname and port values for the relation
+        lucky relation set --relation-id $relation_id \
+            "hostname=$(lucky private-address)" \
+            "port=$(lucky get-config port)"
+    done
+fi
+
+lucky set-status active
